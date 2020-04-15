1de95a8324b9fcb4b4e9da5100814a1ff105a7b3
Jeffry Gonzalez <jeff@hypertheory.com>
2020-04-15 14:52:14 -0400

    Add project files.

---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        
+
+        public int Calculate(string numbers)
+        {
+            return -1;
+        }
+    }
+}
